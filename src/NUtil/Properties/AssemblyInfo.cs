b613255a38d1051488dca903025bf121ebbc3a49
Jerome <jeromerg@gmx.net>
2016-01-29 12:25:26 +0100

    project cleaning, moved documentation to roor

--- src/NUtil/Properties/AssemblyInfo.cs
+++ src/NUtil/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NUtil Library")]
-[assembly: AssemblyDescription("NUtil utility Library")]
+[assembly: AssemblyDescription("NUtil utility Library primarly used in NCase projects")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2fff8a7d-beb6-4c2f-93da-4ca16e5b6598")]
